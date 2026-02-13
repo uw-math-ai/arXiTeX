47ee5be5376b5ed135221f3f7a28fec591b53d74
ericleonen <ericleonen@gmail.com>
2026-02-13 12:44:11 -0800

    Update dependencies

--- arXiTeX/__init__.py
+++ arXiTeX/__init__.py
@@ -2,4 +2,4 @@ from .lib.paper.catalog import paper_catalog
 from .lib.paper.bibliography import parse_bibliography
 from .lib.theorem.parse import parse_paper
 from .lib.slogan.prompt import get_prompt
-# from .lib.embedding.hg_generate import embed_texts_with_hg
+from .lib.embedding.hg_generate import embed_texts_with_hg
